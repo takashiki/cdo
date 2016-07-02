943b6a89f0891bc91dfa4e244a4912c3b782b5ef
takashiki <857995137@qq.com>
2016-07-02 20:37:51 +0800

    增加basePath检测

--- src/Cdo.php
+++ src/Cdo.php
@@ -24,7 +24,9 @@ class Cdo
 
     public function __construct()
     {
-        $this->requestUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $basePath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
+        $requestUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $this->requestUrl = substr($requestUrl, strlen($basePath));
         $this->requestMethod = $_SERVER['REQUEST_METHOD'];
         $this->defaultCallback = [$this, 'notFound'];
     }
